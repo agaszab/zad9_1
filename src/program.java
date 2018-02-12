ea2fe650e6076351580f8fd964ce8b9ff1d404d5
agaszab <gatito@poczta.pl>
2018-02-12 12:30:14 +0100

    usunięcie throws IncorrectAgeException, NameUndefinedException z program.java

--- src/program.java
+++ src/program.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class program {
 
 
-    public static void main(String[] args) throws IncorrectAgeException, NameUndefinedException {
+    public static void main(String[] args)  {
         Scanner scan = new Scanner(System.in);
         String imie;
         String nazwisko;
